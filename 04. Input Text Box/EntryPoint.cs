bf35316ab25c1e69e15fd14b4ff827a13e2ebefe
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2020-03-17 07:49:28 -0500

    Selenium : Input Text & Text Box

---
+++ 04. Input Text Box/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace _04._Input_Text_Box
+{
+    class EntryPoint
+    {
+        static IWebDriver driver = new ChromeDriver();
+        static IWebElement textBox;
+
+        static void Main(string[] args)
+        {
+            string url = "http://testing.todorvachev.com/special-elements/text-input-field/";
+
+            driver.Navigate().GoToUrl(url);
+
+            textBox = driver.FindElement(By.Name("username"));
+            textBox.SendKeys("Test Text");
+            Thread.Sleep(3000);
+
+            Console.WriteLine(textBox.GetAttribute("value"));
+            Thread.Sleep(3000); 
+
+            driver.Quit();
+        }
+    }
+}
---
+++ 05.Check Box/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace _05.Check_Box
+{
+    class EntryPoint
+    {
+        static IWebDriver driver = new ChromeDriver();
+        static IWebElement checkBox;
+
+        static void Main(string[] args)
+        {
+            string url = "http://testing.todorvachev.com/special-elements/check-button-test-3/";
+
+            driver.Navigate().GoToUrl(url);
+
+
+            driver.Quit();
+        }
+    }
+}
