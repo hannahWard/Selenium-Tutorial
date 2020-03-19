043c1dd43d0d2975498a970c07bea6fd13931b07
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2020-03-19 16:01:35 -0500

    Selenium Learning : Radio Buttons/Check Box

--- 05.Check Box/EntryPoint.cs
+++ 05.Check Box/EntryPoint.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace _05.Check_Box
 {
@@ -8,13 +9,44 @@ namespace _05.Check_Box
     {
         static IWebDriver driver = new ChromeDriver();
         static IWebElement checkBox;
+        static IWebElement checkBox2;
 
         static void Main(string[] args)
         {
             string url = "http://testing.todorvachev.com/special-elements/check-button-test-3/";
+            string option = "1";
+            string option2 = "3";
 
             driver.Navigate().GoToUrl(url);
 
+            checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=checkbox]:nth-child(" + option + ")"));
+            checkBox2 = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=checkbox]:nth-child(" + option2 + ")"));
+
+            checkBox.Click();
+            checkBox2.Click();
+
+            
+
+            //display the value of the chosen checkbox
+            //Console.WriteLine(checkBox.GetAttribute("value"));
+
+            //option = "3";
+            //checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=checkbox]:nth-child(" + option + ")"));
+
+            //Console.WriteLine(checkBox.GetAttribute("value"));
+
+
+            //display if chosen option is clicked t/f
+            //if (checkBox.GetAttribute("checked") == "true")
+            //{
+            //    Console.WriteLine("Is Checked");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Checked");
+            //}
+
+            Thread.Sleep(3000);
 
             driver.Quit();
         }
---
+++ 06. Radio Buttons/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace _06._Radio_Buttons
+{
+    class EntryPoint
+    {
+        static IWebDriver driver = new ChromeDriver();
+        static IWebElement radioButton;
+
+        static void Main(string[] args)
+        {
+            string url = "http://testing.todvachev.com/special-elements/radio-button-test/";
+            string option = "1";
+
+            driver.Navigate().GoToUrl(url);
+            radioButton = driver.FindElement(By.CssSelector("#post-10 > div > form > p:nth-child(6) > input[type=radio]:nth-child("+ option +")"));
+
+            if (radioButton.GetAttribute("checked") == "true") 
+            {
+                Console.WriteLine("Is Checked");
+            }
+            else
+            {
+                Console.WriteLine("Not Checked");
+            }
+
+            Thread.Sleep(3000);
+            driver.Quit();
+        }
+    }
+}
