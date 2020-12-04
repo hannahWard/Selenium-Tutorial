dc42a33c02f15fb6f98c1bf203c09cd3fb6e60f5
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2020-12-04 16:43:50 -0600

    Oof should have done this already

--- NameSelector/EntryPoint.cs
+++ NameSelector/EntryPoint.cs
@@ -6,39 +6,56 @@ namespace NameSelector
 {
     class EntryPoint
     {
+        //static void Main()
+        //{
+        //    string url = "http://testing.todorvachev.com/selector/id";
+        //    string ID = "testImage";
+
+        //    IWebDriver driver = new ChromeDriver();
+
+        //    driver.Navigate().GoToUrl(url);
+
+        //    IWebElement element = driver.FindElement(By.Id(ID));
+
+        //    if (element.Displayed)
+        //    {
+        //        GreenMessage("Yes, I can see it");
+        //    }
+        //    else
+        //    {
+        //        RedMessage("Nope, it's not there");
+        //    }
+        //}
+
+        //private static void RedMessage(string message)
+        //{
+        //    Console.ForegroundColor = ConsoleColor.Red;
+        //    Console.WriteLine(message);
+        //    Console.ForegroundColor = ConsoleColor.White;
+        //}
+
+        //private static void GreenMessage(string message)
+        //{
+        //    Console.ForegroundColor = ConsoleColor.Green;
+        //    Console.WriteLine(message);
+        //    Console.ForegroundColor = ConsoleColor.White;
+        //}
+
         static void Main()
         {
-            string url = "http://testing.todorvachev.com/selector/id";
-            string ID = "testImage";
-
             IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://sharx-test.azurewebsites.net");
 
-            driver.Navigate().GoToUrl(url);
-
-            IWebElement element = driver.FindElement(By.Id(ID));
+            IWebElement element = driver.FindElement(By.Name("Login.UserIDMaybe"));
 
             if (element.Displayed)
             {
-                GreenMessage("Yes, I can see it");
+                System.Console.WriteLine("I can see the element");
             }
             else
             {
-                RedMessage("Nope, it's not there");
+                System.Console.WriteLine("Couldn't find the element");
             }
-        }
-
-        private static void RedMessage(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-
-        private static void GreenMessage(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(message);
-            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
